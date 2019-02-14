5707b5db6dec208245ddb2f5dd83f669688b8f8c
Lionel Adams <adamslionel@hotmail.co.uk>
2019-02-14 14:50:22 +0000

    feat: Add jsonwebtoken

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+const TOKEN_SECRET = 'secret_token_key'; //just for PoC, otherwise must be in .env or private variable!
+
+const users = [
+  { id: 1, username: 'admin', password: 'admin' },
+  { id: 2, username: 'guest', password: 'guest' },
+];
 
 app.use(bodyParser.json());
 
@@ -13,9 +20,33 @@ app.get('/status', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  const user = req.body.username;
+  // we need a username and password to login
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send('You must supply a username and password to login');
+    return;
+  }
 
-  res.status(200).send(`You logged in as ${user}`);
+  const { username, password } = req.body;
+
+  const user = users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (!user) {
+    res.status(400).send('User not found');
+    return;
+  }
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      username: user.username,
+    },
+    TOKEN_SECRET,
+    {
+      expiresIn: '3 hours',
+    }
+  );
+  res.status(200).send({ access_token: token });
 });
 
 app.get('*', (req, res) => {
