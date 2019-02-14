b570b823e4b4d480a85e93d70599f9b87b95e6a6
Lionel Adams <adamslionel@hotmail.co.uk>
2019-02-14 14:18:29 +0000

    feat: Add login route and bodyParser dep

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+
+app.use(bodyParser.json());
 
 app.get('/status', (req, res) => {
   const localTime = new Date().toLocaleTimeString();
 
   res.status(200).send(`Server time : ${localTime}`);
+});
+
+app.post('/login', (req, res) => {
+  const user = req.body.username;
+
+  res.status(200).send(`You logged in as ${user}`);
 });
 
 app.get('*', (req, res) => {
