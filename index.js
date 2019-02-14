c91eff99e908200657f047f61c2d13c1cb40914f
Lionel Adams <adamslionel@hotmail.co.uk>
2019-02-14 15:10:19 +0000

    feat: Add express-jwt middleware to validate jsonwebtoken

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const expressjwt = require('express-jwt');
+const cors = require('cors');
 
-const app = express();
 const PORT = process.env.PORT || 8888;
 const TOKEN_SECRET = 'secret_token_key'; //just for PoC, otherwise must be in .env or private variable!
+
+const jwtCheck = expressjwt({
+  secret: TOKEN_SECRET,
+});
 
 const users = [
   { id: 1, username: 'admin', password: 'admin' },
   { id: 2, username: 'guest', password: 'guest' },
 ];
 
-app.use(bodyParser.json());
+const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
+
+/** ROUTING */
+// get: status
 app.get('/status', (req, res) => {
   const localTime = new Date().toLocaleTimeString();
 
   res.status(200).send(`Server time : ${localTime}`);
 });
 
+app.get('/secure', jwtCheck, (req, res) => {
+  res.status(200).send('Secure resource, you should be logged in to see this');
+});
+
+// post: login
 app.post('/login', (req, res) => {
   // we need a username and password to login
   if (!req.body.username || !req.body.password) {
@@ -49,6 +64,7 @@ app.post('/login', (req, res) => {
   res.status(200).send({ access_token: token });
 });
 
+// unhandled routes: send 404
 app.get('*', (req, res) => {
   res.sendStatus(404);
 });
